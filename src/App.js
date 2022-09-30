2974dc4f9163852693770a0aa61b9c847c9cf223
Sem Kholodov <kholodovsem@gmail.com>
2022-09-30 17:37:33 +0300

    root files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {lazy} from 'react';
+import {Routes, Route, Navigate} from 'react-router-dom';
+import {ToastContainer} from 'react-toastify';
+import {Login, Registration, Layout, ProtectedRoute} from 'views';
+import {images, mainImage} from 'helpers/images';
+import {Loader} from 'components';
+import {useLoadMainImage, usePreCacheImage, useGetTechData} from 'hooks';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Rockets = lazy(() => import('views/rockets/rockets.js'));
+const Main = lazy(() => import('views/main/main'));
+const SingleRocket = lazy(() => import('views/single_rocket/single_rocket'));
 
 function App() {
+  const isLogin = useGetTechData();
+  const isLoading = useLoadMainImage(mainImage);
+  usePreCacheImage(images);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route
+            index
+            element={
+              <ProtectedRoute isLogin={isLogin}>
+                {isLoading ? <Loader /> : <Main />}
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/dragons"
+            element={
+              <ProtectedRoute isLogin={isLogin}>
+                <Rockets />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/dragons/:id"
+            element={
+              <ProtectedRoute isLogin={isLogin}>
+                <SingleRocket />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Route>
+      </Routes>
+      <ToastContainer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {Provider} from 'react-redux';
+import {persistor, store} from './store/store';
+import {PersistGate} from 'redux-persist/integration/react';
+import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'modern-normalize/modern-normalize.css';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
+    </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
