dc6418cdf629e9c5dcdf2657289360962d811b54
Sem Kholodov <kholodovsem@gmail.com>
2022-09-30 17:50:53 +0300

    re-export

---
+++ src/views/index.js
@@ -0,0 +1,6 @@
+export {default as Layout} from './layout/layout';
+export {default as SingleRocket} from './single_rocket/single_rocket';
+export {default as AuthPage} from './auth_page/auth_page';
+export {default as ProtectedRoute} from './protected_route/protected_route';
+export {default as Login} from './login/login';
+export {default as Registration} from './registration/registration';
