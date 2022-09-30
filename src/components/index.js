06ca268c702a870630ea3ad3f6d87aa91cc71195
Sem Kholodov <kholodovsem@gmail.com>
2022-09-30 17:48:08 +0300

    re-export

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+export {default as Error} from './error/error';
+export {default as Navigation} from './header/navigation/navigation';
+export {default as Header} from './header/header';
+export {default as Loader} from './loader/loader';
+export {default as Title} from '../views/main/title';
+export {default as Main} from '../views/main/main';
+export {default as Overview} from './overview/overview';
+export {default as Stats} from './overview/stats';
+export {default as Rocket} from './rocket/rocket';
+export {default as Rockets} from '../views/rockets/rockets';
+export {default as Slider} from './slider/slider';
+export {default as PullToRefresh} from './pulltorefresh/pull_to_refresh';
